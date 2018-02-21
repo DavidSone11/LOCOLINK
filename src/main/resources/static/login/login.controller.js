2be82b677439be3d1f04e044679625c5484bcce7
santosh <santosh_citech@live.in>
2018-02-21 20:19:01 +0530

    DSADASDSADASD

--- src/main/resources/static/login/login.controller.js
+++ src/main/resources/static/login/login.controller.js
@@ -1,8 +1,31 @@
 'use strict';
 
-angular.module('LocoLinkApp',[])
-    .controller('loginCtrl', function($scope) {
-        $scope.login = function(username, password){
+var app = angular.module('LocoLinkApp',[
+     'oc.lazyLoad',
+     'ui.router',
+     'ui.bootstrap',
+     'angular-loading-bar',
+     'ngResource',
+     'base64',
+     'spring-data-rest'
+     ]);
 
-        };
+    app.controller('loginCtrl', function($scope,$http, $base64,$window) {
+
+        $scope.login = function(username, password){
+                        $http({
+                            method:'GET',
+                            url:'/',
+                            headers:{
+                                'Authorization':'Basic '+ $base64.encode(username+":"+password)
+
+                            }
+                        }).then(function successCallback(response) {
+                            $window.location.href='/';
+                        }, function errorCallback(response) {
+                            alert("User not authorized.");
+                        });
+                    };
+
+
     });
